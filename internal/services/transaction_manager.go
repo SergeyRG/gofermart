11cf8ef7225c30c7ab74a1df246a071744b93225
Gilmanov Sergey <gsr87@yandex.ru>
2026-05-27 23:00:34 +0300

    Добавить тесты

--- internal/services/transaction_manager.go
+++ internal/services/transaction_manager.go
@@ -2,6 +2,7 @@ package services
 
 import "context"
 
+//go:generate mockgen -destination=../mocks/mock_transaction_manager.go -package=mocks . TransactionManager
 type TransactionManager interface {
 	WithinTransaction(ctx context.Context, fn func(txCtx context.Context) error) error
 }
